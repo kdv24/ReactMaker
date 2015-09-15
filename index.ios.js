68e939815c44832941bde65979caec73b5f8ad7f
Kelly de Vries <kelly@lumenlearning.com>
2015-09-15 13:29:31 -0700

    update loading page

--- index.ios.js
+++ index.ios.js
@@ -71,6 +71,10 @@ var AwesomeProject = React.createClass({
           source={{uri: movie.posters.thumbnail}}
           style={styles.thumbnail}
           />
+          <View style={styles.rightContainer}>
+            <Text style={styles.title}>{movie.title}</Text>
+            <Text style={styles.year}>{movie.year}</Text>
+          </View>
       </View>
     );
   },
